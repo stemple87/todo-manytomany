b7bd9e4799c604f49d84804a91211e21ca4bc9ce
Neil Larion <neil.larion@gmail.com>
2016-02-10 23:27:11 -0800

    Catch todo up to epicodus homework day 2

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,7 +1,5 @@
 using Nancy;
-using Todo.Objects;
 using System.Collections.Generic;
-using Todo.Objects;
 
 namespace ToDoList
 {
@@ -9,21 +7,25 @@ namespace ToDoList
   {
     public HomeModule()
     {
-      Get["/"] = _ => View["add_new_task.cshtml"];
-      Get["/view_all_tasks"] = _ => {
-        List<string> allTasks = Task.GetAll();
-        return View["view_all_tasks.cshtml", allTasks];
+      Get["/"] = _ => {
+        return View["index.cshtml"];
       };
-      Post["/tasks_cleared"] = _ => {
-        Task.ClearAll();
-        return View["tasks_cleared.cshtml"];
+      Get["/tasks"] = _ => {
+        List<Task> allTasks = Task.GetAll();
+        return View["tasks.cshtml", allTasks];
       };
-      Post["/task_added"] = _ => {
-        Task newTask = new Task (Request.Form["new-task"]);
-        newTask.Save();
-        return View["task_added.cshtml", newTask];
+      Get["/tasks/new"] = _ => {
+        return View["task_form.cshtml"];
       };
-
+      Post["/tasks"] = _ => {
+        Task newTask = new Task(Request.Form["new-task"]);
+        List<Task> allTasks = Task.GetAll();
+        return View["tasks.cshtml", allTasks];
+      };
+      Get["/tasks/{id}"] = parameters => {
+  Task task = Task.Find(parameters.id);
+  return View["/task.cshtml", task];
+};
     }
   }
 }
